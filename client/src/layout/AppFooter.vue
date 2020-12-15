643b67d0e02d14ecf85544e1c09ad0488e4deb8a
Thomas Yeum <yeum.thomas@gmail.com>
2020-12-15 02:29:11 -0600

    completed header/footer

--- client/src/layout/AppFooter.vue
+++ client/src/layout/AppFooter.vue
@@ -1,61 +1,102 @@
 <template>
     <footer class="footer has-cards">
-        <div class="container container-lg">
-            <div class="row">
-                <div class="col-md-6 mb-5 mb-md-0">
-                    <div class="card card-lift--hover shadow border-0">
-                        <router-link to="/landing" title="Landing Page">
-                            <img v-lazy="'img/theme/landing.jpg'" class="card-img">
-                        </router-link>
-                    </div>
-                </div>
-                <div class="col-md-6 mb-5 mb-lg-0">
-                    <div class="card card-lift--hover shadow border-0">
-                        <router-link to="/profile" title="Profile Page">
-                            <img v-lazy="'img/theme/profile.jpg'" class="card-img">
-                        </router-link>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="container">
-            <div class="row row-grid align-items-center my-md">
-                <div class="col-lg-6">
-                    <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
-                    <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
-                </div>
-                <div class="col-lg-6 text-lg-center btn-wrapper">
-                    <a target="_blank" rel="noopener" href="https://twitter.com/creativetim"
-                       class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Follow us">
-                        <i class="fa fa-twitter"></i>
-                    </a>
-                    <a target="_blank" rel="noopener" href="https://www.facebook.com/creativetim"
-                       class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Like us">
-                        <i class="fa fa-facebook-square"></i>
-                    </a>
-                    <a target="_blank" rel="noopener" href="https://dribbble.com/creativetim"
-                       class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round" data-toggle="tooltip"
-                       data-original-title="Follow us">
-                        <i class="fa fa-dribbble"></i>
-                    </a>
-                    <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
-                       class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Star on Github">
-                        <i class="fa fa-github"></i>
-                    </a>
-                </div>
+            <div class="row row-eq-height row-grid align-items-center mt-md align-items-stretch">
+                    <div class="col-lg-4">
+                        <div class="row align-items-center text-center">
+                            LOGOGLOOGOGO
+                        </div>
+                        <div class="row">
+                            <h3 class="text-primary font-weight-light mb-2">We are committed to providing you and
+                                your family with high quality dental care in a comfortable setting.</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 text-lg-center">
+                        <h3 class="text-primary mb-2 text-left font-weight-bold">Our Contacts</h3>
+                        <table class="table-sm table table-borderless">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="text-left">
+                                            <i class="ni ni-pin-3"></i> 8126 Long Point Rd, Houston, TX 77055							
+                                            <br>
+                                            <base-button tag="a"
+                                                target="_blank"
+                                                type="primary" 
+                                                href="https://g.page/wooridental?share"
+                                                class="mb-1 mt-1">
+                                                Get directions on map  <i class="fa fa-globe"> </i>
+                                            </base-button>
+                                            </div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="text-left"><i class="fa fa-phone"></i><b> Phone:</b> <a href="tel:8329133879"> 832-913-3879 (Voice)</a></div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="text-left"><i class="fa fa-phone"></i><b> Phone:</b> <a href="tel:7132525148"> 713-252-5148 (Text Only)</a></div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="text-left"><i class="ni ni-email-83"></i><a href="mailto:katyolivedental@yahoo.com"> katyolivedental@yahoo.com</a></div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="text-left"><i class="ni ni-mobile-button"></i> KaKao ID: Redriver23213</div>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-4">
+                        <h3 class="text-primary mb-2 font-weight-bold">Open Hours</h3>
+                        <table class="table-sm table table-striped">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Monday</th>
+                                    <td><b>Closed</b> (Drs work at 2nd office)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Tuesday</th>
+                                    <td><b>9AM - 5:30PM</b> (after-hours available)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Wednesday</th>
+                                    <td><b>Closed</b> (Drs work at 2nd office)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Thursday</th>
+                                    <td><b>9AM - 5:30PM</b> (after-hours available)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Friday</th>
+                                    <td><b>Closed</b> (Drs work at 2nd office)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Saturday</th>
+                                    <td><b>9AM - 4:30PM</b> (opens every other week)</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Sunday</th>
+                                    <td><b>Closed</b></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
             </div>
             <hr>
             <div class="row align-items-center justify-content-md-between">
                 <div class="col-md-6">
                     <div class="copyright">
-                        &copy; {{year}}
-                        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a> & <a href="https://www.binarcode.com" target="_blank" rel="noopener">Binar Code</a>
+                        &copy; {{year}} Teeth Are Us Dental Group.
                     </div>
                 </div>
-                <div class="col-md-6">
+                <!-- <div class="col-md-6">
                     <ul class="nav nav-footer justify-content-end">
                         <li class="nav-item">
                             <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Creative Tim</a>
@@ -72,13 +113,15 @@
                                class="nav-link" target="_blank" rel="noopener">MIT License</a>
                         </li>
                     </ul>
-                </div>
+                </div> -->
             </div>
         </div>
     </footer>
 </template>
 <script>
+import App from '../App.vue';
 export default {
+  components: { App },
   name: 'app-footer',
   data() {
     return {
--- client/src/layout/AppHeader.vue
+++ client/src/layout/AppHeader.vue
@@ -3,62 +3,37 @@
         <base-nav class="navbar-main" transparent type="" effect="light" expand>
             <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
                 <img src="img/brand/white.png" alt="logo">
+                <p>LOGO HERE</p>  <!-- LOGO GOES HERE EVENTUALLY -->
             </router-link>
 
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
+            <ul class="navbar-nav navbar-nav-hover align-items-lg-center ml-lg-auto">
+                <li class="nav-item">
+                    <a slot="title" href="#" class="nav-link" role="button">
+                        <i class="fa fa-home"></i>
+                        <span class="nav-link-inner--text">HOME</span>
                     </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
-            </div>
-
-            <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-                <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
-                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
-                        <i class="ni ni-ui-04 d-lg-none"></i>
-                        <span class="nav-link-inner--text">Components</span>
+                </li>
+                <li class="nav-item">
+                    <a slot="title" href="#" class="nav-link" role="button">
+                        <i class="fa fa-user"></i>
+                        <span class="nav-link-inner--text">ABOUT US</span>
                     </a>
-                    <div class="dropdown-menu-inner">
-                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                           class="media d-flex align-items-center">
-                            <div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                                <i class="ni ni-spaceship"></i>
-                            </div>
-                            <div class="media-body ml-3">
-                                <h6 class="heading text-primary mb-md-1">Getting started</h6>
-                                <p class="description d-none d-md-inline-block mb-0">Get started with Bootstrap, the
-                                    world's most popular framework for building responsive sites.</p>
-                            </div>
-                        </a>
-                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                           class="media d-flex align-items-center">
-                            <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                                <i class="ni ni-ui-04"></i>
-                            </div>
-                            <div class="media-body ml-3">
-                                <h5 class="heading text-warning mb-md-1">Components</h5>
-                                <p class="description d-none d-md-inline-block mb-0">Learn how to use Argon
-                                    compiling Scss, change brand colors and more.</p>
-                            </div>
-                        </a>
-                    </div>
-                </base-dropdown>
-                <base-dropdown tag="li" class="nav-item">
-                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
-                        <i class="ni ni-collection d-lg-none"></i>
-                        <span class="nav-link-inner--text">Examples</span>
+                </li>
+                <li class="nav-item">
+                    <a slot="title" href="#" class="nav-link" role="button">
+                        <i class="fa fa-check"></i>
+                        <span class="nav-link-inner--text">OUR SERVICES</span>
                     </a>
-                    <router-link to="/landing" class="dropdown-item">Landing</router-link>
-                    <router-link to="/profile" class="dropdown-item">Profile</router-link>
-                    <router-link to="/login" class="dropdown-item">Login</router-link>
-                    <router-link to="/register" class="dropdown-item">Register</router-link>
-                </base-dropdown>
+                </li>
+                <li class="nav-item">
+                    <a slot="title" href="#" class="nav-link" role="button">
+                        <i class="fa fa-phone"></i>
+                        <span class="nav-link-inner--text">CONTACT US</span>
+                    </a>
+                </li>
             </ul>
-            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+        
+            <!-- <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
                     <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank" rel="noopener"
                        data-toggle="tooltip" title="Like us on Facebook">
@@ -96,7 +71,7 @@
                         <span class="nav-link-inner--text">Download</span>
                     </a>
                 </li>
-            </ul>
+            </ul> -->
         </base-nav>
     </header>
 </template>
@@ -104,12 +79,14 @@
 import BaseNav from "@/components/BaseNav";
 import BaseDropdown from "@/components/BaseDropdown";
 import CloseButton from "@/components/CloseButton";
+import App from '../App.vue';
 
 export default {
   components: {
     BaseNav,
     CloseButton,
-    BaseDropdown
+    BaseDropdown,
+    App
   }
 };
 </script>
